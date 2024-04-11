bc5e0b6ac00330ae2cc21a17f07585f722c88f44
Alexx341 <Piecuchaleksander@gmail.com>
2024-04-11 22:43:34 +0200

    Server

--- server.py
+++ server.py
@@ -3,38 +3,52 @@ import socket
 import threading 
 
 IP = socket.gethostbyname(socket.gethostname())
+print(IP)
 PORT = 5566
 ADDR = (IP,PORT)
 SIZE = 1024
 FORMAT = "utf-8"
-DISCONNECT_MSG = "/D"
+DISCONNECT_MSG = "/disc"
+connected_clients = []
 def handle_client(conn, addr):
+    connected_clients.append(conn)
+    if threading.active_count() - 1 == 1:
+        msg = "You're the first one connected, wait for the second player"
+        conn.send(msg.encode(FORMAT))    
+    else:
 
+        msg = "Both players connected"
+        for client in connected_clients:
+            print(client)
+            client.send(msg.encode(FORMAT))
+
+    
     print(f"[NEW CONNECTION] {addr} connected")
     connected = True
     while connected:
+
         msg = conn.recv(SIZE).decode(FORMAT)
         if msg == DISCONNECT_MSG:
             connected = False
         
-        print(f"[{addr,}] {msg}")
-        msg = f"Msg received: {msg}"
-        conn.send(msg.encode(FORMAT))
 
     conn.close()
-
+    connected_clients.remove(conn)
 def main():
     print("[STARTING] server is starting")
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(ADDR)
-    server.listen()
+    server.listen(2)
     print(f"[LISTENING] server is listening on {IP}:{PORT}")
 
     while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handle_client, args=(conn,addr))
-        thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+        if threading.active_count() - 1<2:
+            conn, addr = server.accept()
+            thread = threading.Thread(target=handle_client, args=(conn,addr))
+            thread.start()
+            print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+
 
 if __name__ == "__main__":
     main()
