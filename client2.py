da0fbb65db82aae2d21b8a5e38305b752b011514
Alexx341 <Piecuchaleksander@gmail.com>
2024-04-11 22:44:48 +0200

    clients

--- client2.py
+++ client2.py
@@ -1,26 +1,32 @@
 # Python Client
 import socket
-IP = socket.gethostbyname(socket.gethostname())
+IP = "192.168.100.18"
 PORT = 5566
 ADDR = (IP,PORT)
 SIZE = 1024
 FORMAT = "utf-8"
-DISCONNECT_MSG = "/D"
+DISCONNECT_MSG = "/disc"
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(ADDR)
     print(f"[CONNECTED] client connected to server at {IP}:{PORT}")
     connected = True
     while connected:
+        msg = client.recv(SIZE).decode(FORMAT)
+        if msg == "You're the first one connected, wait for the second player":
+            print(f"[SERVER] {msg}")
+            msg = client.recv(SIZE).decode(FORMAT)
+            print(f"[SERVER] {msg}")
+        else:
+            print(f"[SERVER] {msg}")
+        
+        
         msg = input("> ")
-
-        client.send(msg.encode(FORMAT))
-
         if msg == DISCONNECT_MSG:
             connected = False
         else:
-            msg = client.recv(SIZE).decode(FORMAT)
-            print(f"[SERVER] {msg}")
+            client.send(msg.encode(FORMAT))
+
 
 
 if __name__ == "__main__":
