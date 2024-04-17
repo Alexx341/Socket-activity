0eb4ffa4c4948d59088875e763a6604974919443
Alexx341 <Piecuchaleksander@gmail.com>
2024-04-17 19:42:58 +0200

    fin

--- network.py
+++ network.py
@@ -1,26 +1,20 @@
 import socket
 
 class Network:
-    def __init__(self):
-        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server = "192.168.239.53"
-        self.port = 5555
-        self.addr = (self.server, self.port)
-        self.pos = self.connect()
+    def __init__(this):
+        this.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        this.server = "192.168.100.18"
+        this.port = 5555
+        this.addr = (this.server, this.port)
+        this.pos = this.connect()
     
-    def getPos(self):
-        return self.pos
-    
-    def connect(self):
-        try:
-            self.client.connect(self.addr)
-            return self.client.recv(2048).decode()
-        except:
-            pass
-    def send(self, data):
-        try:
-            self.client.send(str.encode(data))
-            return self.client.recv(2048).decode()
-        except socket.error as e:
-            print(e)
+    def getPos(this):
+        return this.pos
+    def connect(this):
+            this.client.connect(this.addr)
+            return this.client.recv(2048).decode()
+    def send(this, data):
+            this.client.send(str.encode(data))
+            return this.client.recv(2048).decode()
+
 
--- server.py
+++ server.py
@@ -1,25 +1,20 @@
 import socket
 from  _thread import *
-import sys
 
-server = "192.168.239.53"
+server = "192.168.100.18"
 port = 5555
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-try:
-    s.bind((server, port))
-except socket.error as e:
-    str(e)
-
+s.bind((server, port))
 s.listen(2)
-print("waiting for connection, server started")
+print("Serwer stoi, czeka na polaczenie")
 
-def read_pos(str):
-    str = str.split(",")
-    return int(str[0]), int(str[1])
+def read_pos(pos):
+    pos = pos.split(",")
+    return int(pos[0]), int(pos[1])
 
-def make_pos(tup):
-    return str(tup[0]) + "," + str(tup[1])
+def make_pos(data):
+    return str(data[0]) + "," + str(data[1])
 
 pos = [(0,0),(100,100)]
 
@@ -27,32 +22,28 @@ def threaded_client(conn, player):
     conn.send(str.encode(make_pos(pos[player])))
     reply = ""
     while True:
-        try:
-            data = read_pos(conn.recv(2048).decode())
-            pos[player] = data
+    
+        data = read_pos(conn.recv(2048).decode())
+        pos[player] = data
 
-            if not data:
-                print("Disconnected")
-                break
+        if not data:
+            print("Koniec polaczenia")
+            break
+        else:
+            if player == 1:
+                reply = pos[0]
             else:
-                if player == 1:
-                    reply = pos[0]
-                else:
-                    reply = pos[1]
-                print("Received: ", data)
-                print("Sending ", reply)
+                reply = pos[1]
+            print("Received: ", data)
+            print("Sending ", reply)
             
             conn.sendall(str.encode(make_pos(reply)))
-        except:
-            break
-
-    print("Lost connection")
+    print("Polaczenie utracone")
     conn.close()
 
-currentPlayer = 0
+PlayerCount = 0
 while True:
     conn, addr = s.accept()
     print("Connected to: ", addr)
-
-    start_new_thread(threaded_client, (conn, currentPlayer))
-    currentPlayer += 1
+    start_new_thread(threaded_client, (conn, PlayerCount))
+    PlayerCount += 1
